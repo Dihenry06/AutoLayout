d751e7e5bf6b60301a0775301f3186df8156ae35
Dihenry06 <dihenry06@gmail.com>
2020-07-29 23:14:20 -0300

    Width and Height Constraint, Vertical and Horizontal Alignment

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -20,10 +20,16 @@ class ViewController: UIViewController {
         
         RedView.translatesAutoresizingMaskIntoConstraints = false
         
-        RedView.topAnchor.constraint(equalTo:view.topAnchor,constant: 50).isActive = true
-        RedView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -50).isActive = true
-        RedView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 50).isActive = true
-        RedView.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: -50).isActive = true
+//        RedView.topAnchor.constraint(equalTo:view.topAnchor,constant: 50).isActive = true
+//        RedView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -50).isActive = true
+//        RedView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 50).isActive = true
+//        RedView.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: -50).isActive = true
+        
+        RedView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        RedView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        RedView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        RedView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
     }
 }
