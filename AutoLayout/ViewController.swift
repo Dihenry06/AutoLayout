7897f523d618f086a2cd484d656aa1ebed24e88d
Dihenry06 <dihenry06@gmail.com>
2020-07-29 23:40:11 -0300

    view split half and half

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -16,20 +16,26 @@ class ViewController: UIViewController {
         let RedView = UIView()
         RedView.backgroundColor = .red
         
+        let BlueView = UIView()
+        BlueView.backgroundColor = .blue
+        
         view.addSubview(RedView)
+        view.addSubview(BlueView)
         
         RedView.translatesAutoresizingMaskIntoConstraints = false
+        BlueView.translatesAutoresizingMaskIntoConstraints = false
         
-//        RedView.topAnchor.constraint(equalTo:view.topAnchor,constant: 50).isActive = true
-//        RedView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -50).isActive = true
-//        RedView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 50).isActive = true
-//        RedView.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: -50).isActive = true
+        RedView.topAnchor.constraint(equalTo: view.topAnchor,constant: 0).isActive = true
+        RedView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 0).isActive = true
+        RedView.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: 0).isActive = true
         
-        RedView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        RedView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        RedView.widthAnchor.constraint(equalToConstant: view.bounds.width / 2).isActive = true
         
-        RedView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        RedView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        BlueView.topAnchor.constraint(equalTo: view.topAnchor,constant: 0).isActive = true
+        BlueView.leadingAnchor.constraint(equalTo: RedView.trailingAnchor, constant: 0).isActive = true
+        BlueView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        BlueView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
         
+//        let stackView : UIStackView = UIStackView(arrangedSubviews: [RedView,BlueView])
     }
 }
